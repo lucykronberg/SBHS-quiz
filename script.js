ef820c20e62dfedc847158f7a33ef1d533e46850
lucykronberg <lucykronberg@gmail.com>
2024-09-03 13:43:50 -0700

    hide and show answers to the questions

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
 	$("#para1").click(function(){
-	$("p1").toggle();
+		$("#p1").toggle();
 	});
+	$("#para2").click(function(){
+		$("#p2").toggle();
+	});
+	$("#para3").click(function(){
+		$("#p3").toggle();
+	});
+	$("#para4").click(function(){
+		$("#p4").toggle();
+	});
+	$("#para5").click(function(){
+		$("#p5").toggle();
+	});
+});
