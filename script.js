9a225e1735bc015b1b5f354ef99c3e9432cb38b3
lucykronberg <lucykronberg@gmail.com>
2024-09-05 13:27:29 -0700

    add finishing touches

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 $(document).ready(function() {
-	$("#para1").click(function(){
-		$("#p1").toggle();
+	$(".questions").click(function(){
+		$(this).next().toggle();
 	});
-	$("#para2").click(function(){
-		$("#p2").toggle();
-	});
-	$("#para3").click(function(){
-		$("#p3").toggle();
-	});
-	$("#para4").click(function(){
-		$("#p4").toggle();
-	});
-	$("#para5").click(function(){
-		$("#p5").toggle();
-	});
+	
 });
